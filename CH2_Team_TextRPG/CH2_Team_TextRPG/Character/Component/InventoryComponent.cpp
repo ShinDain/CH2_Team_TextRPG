48957e8388d9998285dc6b25bbe366fcc2af9128
Shin Dain <qwe3546@naver.com>
2026-05-25 11:10:26 +0900

    feat : InventoryComponent 기본 구성

---
+++ CH2_Team_TextRPG/CH2_Team_TextRPG/Character/Component/InventoryComponent.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "InventoryComponent.h"
+
+InventoryComponent::InventoryComponent(Object* InOwner)
+	:Component(InOwner, "Inventory")
+{
+}
+
+InventoryComponent::~InventoryComponent()
+{
+}
+
+bool InventoryComponent::Initialize()
+{
+    return false;
+}
+
+bool InventoryComponent::UseItem()
+{
+	return false;
+}
+
+void InventoryComponent::AcqireItem(const std::string ItemName)
+{
+}
+
+std::vector<const std::string&> InventoryComponent::GetItemList()
+{
+	return std::vector<const std::string&>();
+}
---
+++ CH2_Team_TextRPG/CH2_Team_TextRPG/Character/Component/InventoryComponent.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "Core/Component.h"
+#include "Data/Table/ItemDataTable.h"
+
+struct InventoryEntry
+{
+	int Id;
+	int Amount;
+	std::string Name;
+};
+
+class InventoryComponent : public Component
+{
+public:
+	InventoryComponent(Object* InOwner);
+	virtual ~InventoryComponent() override;
+
+	bool Initialize() override;
+	
+	bool UseItem();
+
+	void AcqireItem(const std::string ItemName);
+	std::vector<const std::string&> GetItemList();
+
+private:
+	std::vector<InventoryEntry> Entries;
+	std::unordered_map<std::string, InventoryEntry&> NameMap;
+	std::unordered_map<int, InventoryEntry&> IdMap;
+};
+
