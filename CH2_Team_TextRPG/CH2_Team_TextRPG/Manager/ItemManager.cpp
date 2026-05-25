5f1a6776c1230d88a84d4be2a385edfdf7e82ec3
Shin Dain <qwe3546@naver.com>
2026-05-25 17:35:49 +0900

    feat : Item을 관리하는 ItemManager 추가

--- CH2_Team_TextRPG/CH2_Team_TextRPG/Manager/ItemManager.cpp
+++ CH2_Team_TextRPG/CH2_Team_TextRPG/Manager/ItemManager.cpp
@@ -1,9 +1,8 @@
 ﻿#include "pch.h"
 #include "ItemManager.h"
+#include "Data/Table/ItemDataTable.h"
 #include "Effect/Factory/EffectFactory.h"
-#include "Effect/Effect_Heal.h"
-
-#include <functional>
+#include "Item/Item.h"
 
 ItemManager::ItemManager()
 {
@@ -11,4 +10,77 @@ ItemManager::ItemManager()
 
 ItemManager::~ItemManager()
 {
+	for (Item* item : Items)
+	{
+		delete item;
+		item = nullptr;
+	}
+
+	Items.clear();
+	NameMap.clear();
+	IdMap.clear();
+}
+
+ItemManager& ItemManager::GetInstance()
+{
+	static ItemManager Instance;
+	return Instance;
+}
+
+Item* ItemManager::FindItemByName(const std::string& Name)
+{
+	if (NameMap.find(Name) == NameMap.end())
+	{
+		const ItemData* Data = FindItemDataByName(Name);
+		Item* newItem = CreateItemInstance(Data);
+
+		return newItem;
+	}
+	else
+	{
+		auto iter = NameMap.find(Name);
+		return iter->second;
+	}
+
+	return nullptr;
+}
+
+Item* ItemManager::FindItemById(uint32_t Id)
+{
+	if (IdMap.find(Id) == IdMap.end())
+	{
+		const ItemData* Data = FindItemDataById(Id);
+		Item* newItem = CreateItemInstance(Data);
+
+		return newItem;
+	}
+	else
+	{
+		auto iter = IdMap.find(Id);
+		return iter->second;
+	}
+
+	return nullptr;
+}
+
+Item* ItemManager::CreateItemInstance(const ItemData* Data)
+{
+	Item* newItem = new Item(Data);
+	
+	if (newItem)
+	{
+		for (const auto& tagData : Data->EffectTags)
+		{
+			newItem->AddEffect(EffectFactory::CreateEffect(tagData.Tag, tagData.Value));
+		}
+
+		std::string Name = Data->Name;
+		uint32_t Id = Data->Id;
+
+		Items.emplace_back(newItem);
+		NameMap[Name] = newItem;
+		IdMap[Id] = newItem;
+	}
+	
+	return newItem;
 }
--- CH2_Team_TextRPG/CH2_Team_TextRPG/Manager/ItemManager.h
+++ CH2_Team_TextRPG/CH2_Team_TextRPG/Manager/ItemManager.h
@@ -5,9 +5,29 @@ class ItemManager
 {
 public:
 	ItemManager();
-	virtual ~ItemManager();
+	~ItemManager();
+
+	static ItemManager& GetInstance();
+
+	Item* FindItemByName(const std::string& Name);
+	Item* FindItemById(uint32_t Id);
 
 private:
+	Item* CreateItemInstance(const ItemData* Data);
 
+private:
+	std::vector<Item*> Items;
+
+	std::unordered_map<std::string, Item*> NameMap;
+	std::unordered_map<uint32_t, Item*> IdMap;
 };
 
+inline Item* FindItemByName(const std::string& Name)
+{
+	return ItemManager::GetInstance().FindItemByName(Name);
+}
+
+inline Item* FindItemById(uint32_t Id)
+{
+	return ItemManager::GetInstance().FindItemById(Id);
+}
