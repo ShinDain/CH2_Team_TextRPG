09a3e979fe9c2cbc5901265383f7e0e5bd900de8
chwy <dndus310@gmail.com>
2026-05-26 22:08:19 +0900

    feat: EquipmentComponent -> 슬롯 등록, 해제, 모든 슬롯 해제, 기능 추가

--- CH2_Team_TextRPG/CH2_Team_TextRPG/Character/Component/EquipmentComponent.cpp
+++ CH2_Team_TextRPG/CH2_Team_TextRPG/Character/Component/EquipmentComponent.cpp
@@ -1,19 +1,135 @@
 #include "pch.h"
 #include "EquipmentComponent.h"
 
+#include "StatComponent.h"
+#include "Data/Table/ItemDataTable.h"
+#include "Enum/EItemType.h"
+#include "Item/Item.h"
+#include "Effect/Effect.h"
+#include "Effect/Factory/EffectFactory.h"
+
+
+
 EquipmentComponent::EquipmentComponent(Object* InOwner) :
 	Component(InOwner, "Equipment")
 {
-	
 }
 
 EquipmentComponent::~EquipmentComponent()
 {
-	
 }
 
 bool EquipmentComponent::Initialize()
 {
-	// Not Implement.
-	return false;
+	Slots.reserve(static_cast<size_t>(EEquipmentType::End));
+	
+#if DEBUG_CODE
+	EffectData D = EffectData();
+	D.Tag = "Heal";
+	D.Value = 30;
+	Effect* NewEffect = EffectFactory::CreateEffect(D);
+	if (!NewEffect) 
+		return false;
+	
+	NewEffect->Apply(Owner, {});
+	delete NewEffect;
+#endif
+	
+	return true;
+}
+
+bool EquipmentComponent::Equip(Item* InItem)
+{
+	if (!InItem) return false;
+
+	const ItemData* ItemData = InItem->GetItemData();
+	bool ItemValidate = !ItemData || ItemData->Category != EItemCategory::Equipment; 
+	if (ItemValidate)
+	{
+		return false;
+	}
+
+	const FEquipmentItemData* EquipData = EquipmentDataTable::GetInstance().FindEquipmentDataByIndex(ItemData->Id);
+	if (!EquipData)
+	{
+		return false;
+	}
+
+	const EEquipmentType Slot = EquipData->EquipmentType;
+	if (auto It = Slots.find(Slot); It != Slots.end() && It->second != nullptr)
+	{
+		Unequip(Slot);
+	}
+
+	for (const auto& EffectData : EquipData->EffectDatas)
+	{
+		Effect* NewEffect = EffectFactory::CreateEffect(EffectData);
+		if (!NewEffect) continue;
+		NewEffect->Apply(Owner, {});
+		delete NewEffect;
+	}
+
+	Slots[Slot] = InItem;
+
+	return true;
+}
+
+void EquipmentComponent::Unequip(EEquipmentType Slot)
+{
+	auto It = Slots.find(Slot);
+	if (It == Slots.end() || It->second == nullptr)
+	{
+		return;
+	}
+
+	Item* SlotItem = It->second;
+
+	const ItemData* ItemData = SlotItem->GetItemData();
+	if (ItemData)
+	{
+		const FEquipmentItemData* EquipData = EquipmentDataTable::GetInstance().FindEquipmentDataByIndex(ItemData->Id);
+		if (!EquipData)
+		{
+			return;
+		}
+		
+		for (const auto& EffData : EquipData->EffectDatas)
+		{
+			Effect* NewEffect = EffectFactory::CreateEffect(EffData);
+			if (!NewEffect) continue;
+			NewEffect->Remove(Owner, {});
+			delete NewEffect;
+			NewEffect = nullptr;
+		}
+	}
+	Slots.erase(It);
+}
+
+void EquipmentComponent::UnequipAll()
+{
+	std::vector<EEquipmentType> AllSlots;
+	AllSlots.reserve(Slots.size());
+	for (const auto& Pair : Slots)
+	{
+		AllSlots.push_back(Pair.first);
+	}
+	for (EEquipmentType Slot : AllSlots)
+	{
+		Unequip(Slot);
+	}
+}
+
+void EquipmentComponent::ApplySlotItem(const Item& InItem)
+{
+	
+}
+
+Item* EquipmentComponent::FindItemFromSlot(EEquipmentType Slot)
+{
+	auto It = Slots.find(Slot);
+	if (It != Slots.end())
+	{
+		return It->second;
+	}
+	return nullptr;
 }
--- CH2_Team_TextRPG/CH2_Team_TextRPG/Character/Component/EquipmentComponent.h
+++ CH2_Team_TextRPG/CH2_Team_TextRPG/Character/Component/EquipmentComponent.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "Core/Component.h"
 
+class Item;
+enum class EEquipmentType;
 class EquipmentComponent : public Component
 {
 public:
@@ -8,4 +10,15 @@ public:
 	~EquipmentComponent() override;
 	
 	bool Initialize() override;
+	
+	bool Equip(Item* InItem);
+	void Unequip(EEquipmentType Slot);
+	void UnequipAll();
+
+	void ApplySlotItem(const Item& InItem);
+	
+	Item* FindItemFromSlot(EEquipmentType Slot);
+	
+private:
+	std::unordered_map<EEquipmentType, Item*> Slots;
 };
