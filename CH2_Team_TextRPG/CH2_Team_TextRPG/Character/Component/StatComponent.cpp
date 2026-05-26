3630a08bfd4fd174d4ad1ef6621ff14e4e97fe36
chwy <dndus310@gmail.com>
2026-05-26 14:38:44 +0900

    feat: StatComponent 기본 기능(Get,Set,Add) 추가

--- CH2_Team_TextRPG/CH2_Team_TextRPG/Character/Component/StatComponent.cpp
+++ CH2_Team_TextRPG/CH2_Team_TextRPG/Character/Component/StatComponent.cpp
@@ -1,9 +1,12 @@
 #include "pch.h"
 #include "StatComponent.h"
+#include "Data/Character/Stat.h"
 
 StatComponent::StatComponent(Object* InOwner) :
-	Component(InOwner, "Stat")
+	Component(InOwner, "Stat"),
+	Stats(static_cast<int>(EStatType::Max))
 {
+	
 }
 
 StatComponent::~StatComponent()
@@ -11,8 +14,32 @@ StatComponent::~StatComponent()
 	
 }
 
+const size_t StatComponent::StatTypeToIndex(EStatType InType)
+{
+	const size_t Idx = static_cast<size_t>(InType);
+	assert(Idx < static_cast<size_t>(EStatType::Max) && "EStatType 범위 초과");
+	return Idx;
+}
+
+int StatComponent::GetStat(EStatType InType)
+{
+	const size_t Idx = StatTypeToIndex(InType);
+	return Stats[Idx];
+}
+
+void StatComponent::SetStat(EStatType InType, int Value)
+{
+	const size_t Idx = StatTypeToIndex(InType);
+	Stats[Idx] = Value;
+}
+
+void StatComponent::AddStat(EStatType InType, int Delta)
+{
+	const size_t Idx = StatTypeToIndex(InType);
+	Stats[Idx] += Delta;
+}
+
 bool StatComponent::Initialize()
 {
-	// Not Implement.
-	return false;
+	return true;
 }
--- CH2_Team_TextRPG/CH2_Team_TextRPG/Character/Component/StatComponent.h
+++ CH2_Team_TextRPG/CH2_Team_TextRPG/Character/Component/StatComponent.h
@@ -1,11 +1,22 @@
 #pragma once
 #include "Core/Component.h"
 
+enum class EStatType;
 class StatComponent : public Component
 {
 public:
 	StatComponent(Object* InOwner);
 	~StatComponent() override;
+
+	int GetStat(EStatType InType);
+	void SetStat(EStatType InType, int Value);
+	void AddStat(EStatType InType, int Delta);
 	
 	bool Initialize() override;
+	
+private:
+	const size_t StatTypeToIndex(EStatType InType);
+	
+private:
+	std::vector<int> Stats;
 };
